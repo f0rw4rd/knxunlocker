d75700baa60205219d0060b906f6035bed125099
f0rw4rd <f0rw4rd@users.noreply.github.com>
2023-02-19 12:12:57 +0100

    Fixed gitignore and removed pdb from release

--- release_helper.sh
+++ release_helper.sh
@@ -7,5 +7,5 @@ rm -rf bin/
 dotnet publish -c Release -r win-x64 --self-contained
 dotnet publish -c Release -r linux-x64 --self-contained
 
-(cd  bin/Release/net6.0/win-x64/publish  && zip -9 "../../../../../knxunlocker_win_$tag.zip" *)
-(cd  bin/Release/net6.0/linux-x64/publish  && zip -9 "../../../../../knxunlocker_linux_$tag.zip" *)
+(cd  bin/Release/net6.0/win-x64/publish && rm knxunlocker.pdb  && zip -9 "../../../../../knxunlocker_win_$tag.zip" *)
+(cd  bin/Release/net6.0/linux-x64/publish && rm knxunlocker.pdb && zip -9 "../../../../../knxunlocker_linux_$tag.zip" *)
